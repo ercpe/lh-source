4d63b290654e64350dcd36d261e1d97b0eaf6818
Justin Lecher <jlec@gentoo.org>
2014-11-09 15:33:29 +0100

    pep8
    Signed-off-by: Justin Lecher <jlec@gentoo.org>

--- app-admin/admin-tools/adduse.py
+++ app-admin/admin-tools/adduse.py
@@ -21,14 +21,16 @@ if isinstance(_cp, dict):
 	cp = _cp['dots_disallowed_in_PN']
 else:
 	cp = _cp
-uses_re = re.compile("^\s*" + '(?P<catpkg>' + _verrule + cp + ')' + " (?P<useflags>(.*))", re.IGNORECASE)
+uses_re = re.compile(
+	"^\s*" + '(?P<catpkg>' + _verrule + cp + ')' + " (?P<useflags>(.*))", re.IGNORECASE
+)
 
 
 class PackageUSEEntry(object):
 	comments = []
 	package = None
 	uses = []
-		
+
 	def __init__(self, package=None, uses=[], comments=[]):
 		self.comments = comments
 		self.package = package
@@ -37,14 +39,15 @@ class PackageUSEEntry(object):
 	def __cmp__(self, other):
 		left = self.package.lstrip("<>=")
 		right = other.package.lstrip("<>=")
-		return cmp(left, right) # this does not take account of the version numbers, but thats ok 
+		# this does not take account of the version numbers, but thats ok
+		return cmp(left, right)
 
 
 class PackageUSEHandler(object):
 	_package_use_file = None
 	_entries = []
 	_garbage = []
-	
+
 	def __init__(self, file=None):
 
 		if file:
@@ -54,37 +57,41 @@ class PackageUSEHandler(object):
 			prefix_root = portage.settings['EPREFIX']
 			if not prefix_root.strip():
 				prefix_root = "/"
-			self._package_use_file = os.path.join(prefix_root, "etc/portage/package.use")
-		
+			self._package_use_file = os.path.join(
+				prefix_root, "etc/portage/package.use"
+				)
+
 		self.read()
 
 	def read(self):
 		"""Reads the list of packages/use flags from the package.use file"""
-		
+
 		if not os.path.exists(self._package_use_file):
 			return
-		
+
 		if not os.access(self._package_use_file, os.R_OK):
 			raise IOError("Cannot read file %s" % self._package_use_file)
-		
+
 		comments = []
-		
+
 		with open(self._package_use_file) as f:
 			for l in f:
 				line = l.strip()
 
 				if not line:
-					continue # skip empty lines
-				
+					continue  # skip empty lines
+
 				if comment_re.match(line):
 					comments.append(line)
 					continue
 
-				match = uses_re.match(line)			
+				match = uses_re.match(line)
 				if not match:
-					self._garbage.append(line) # the line is not a comment and not a valid entry for the package.use file
+					# the line is not a comment and not a valid entry for the
+					# package.use file
+					self._garbage.append(line)
 					continue
-													
+
 				pkg = match.group('catpkg')
 				uses = re.split("\s+", match.group('useflags'))
 
@@ -94,7 +101,9 @@ class PackageUSEHandler(object):
 
 				try:
 					# this test matches only duplicate strings
-					entry = (x for x in self._entries if x.package == pkg).next()
+					entry = (
+						x for x in self._entries if x.package == pkg
+						).next()
 					entry.comments.extend(comments)
 					entry.uses.extend(uses)
 				except StopIteration:
@@ -103,11 +112,10 @@ class PackageUSEHandler(object):
 
 				comments = []
 
-
 		if comments:
-			# comments at the end of the file (without a following package specification)
+			# comments at the end of the file (without a following package
+			# specification)
 			self._garbage.extend(comments)
-
 
 	def write(self):
 		"""Writes a sorted, cleaned list of packages/use flags to the package.use file"""
@@ -119,12 +127,12 @@ class PackageUSEHandler(object):
 					os.makedirs(directory, 0755)
 				except:
 					raise IOError("Could not create directory %s!" % directory)
-	
+
 		if os.path.exists(self._package_use_file) and not os.access(self._package_use_file, os.W_OK):
-			raise IOError("You are not allowed to write to %s." % (self._package_use_file))
+			raise IOError("You are not allowed to write to %s." %
+						  (self._package_use_file))
 
-
- 		# sort the entries (uses the __cmp__() function on the entries object)
+		# sort the entries (uses the __cmp__() function on the entries object)
 		self._entries = sorted(self._entries)
 
 		with open(self._package_use_file, 'w') as o:
@@ -135,7 +143,6 @@ class PackageUSEHandler(object):
 					o.write("%s %s\n" % (pkg.package, ' '.join(pkg.uses)))
 
 			o.writelines('\n'.join(self._garbage) + '\n')
-			
 
 	def set_use(self, pkg, uses):
 		try:
@@ -146,15 +153,17 @@ class PackageUSEHandler(object):
 			self._entries.append(entry)
 
 		if not re.match(r'^' + _verrule, pkg).group(1):
-			# we set the use flag for foo/bar baz - change the use flag on all possible version dependent entries
+			# we set the use flag for foo/bar baz - change the use flag on all
+			# possible version dependent entries
 			for p in [x.package for x in self._entries]:
-				if re.match("^" + _verrule, p).group(1) and pkg in p: # pkg in p may be to fuzzy
+				# pkg in p may be to fuzzy
+				if re.match("^" + _verrule, p).group(1) and pkg in p:
 					self.remove_use(p, uses)
 
 	def remove_use(self, pkg, uses):
 		try:
 			entry = (x for x in self._entries if x.package == pkg).next()
-			
+
 			l = uses
 			l.extend([self._negate(x) for x in uses])
 			for u in l:
@@ -162,7 +171,6 @@ class PackageUSEHandler(object):
 					entry.uses.remove(u)
 		except StopIteration:
 			pass
-
 
 	def _negate(self, use):
 		if use.startswith("-"):
@@ -172,7 +180,7 @@ class PackageUSEHandler(object):
 
 	def _clean_uses(self, uses):
 		newuses = []
-	
+
 		for current in uses:
 			neg_use = self._negate(current)
 
@@ -198,31 +206,33 @@ def test_package(package):
 		# JS, 2011-04-08: slot in cp (#124)
 		pkg = pkg[:pkg.index(':')]
 	else:
-		vm = re.match("^" + _verrule, pkg) # < or > or =
+		vm = re.match("^" + _verrule, pkg)  # < or > or =
 		if vm and vm.group(1):
-			m = re.match("^" + _verrule + r'([\w+][\w+.-]*/[\w\-]+)' + _vr, pkg)
+			m = re.match(
+				"^" + _verrule + r'([\w+][\w+.-]*/[\w\-]+)' + _vr, pkg
+				)
 			if not m:
 				raise Exception("Invlid cpv")
 			pkg = m.group(2).strip("-")
 
-	if pkg in cp_all: # exact match for cp
+	if pkg in cp_all:  # exact match for cp
 		return package
-	
+
 	# no exact match found. let the user choose
 	matches = [x for x in cp_all if pkg in x]
-	
+
 	if len(matches) > 0:
 		print "Package name does not exist. Possible packages:"
-		print '\n'.join(["[%s] %s" % (i+1, matches[i]) for i in xrange(0, len(matches))])
+		print '\n'.join(["[%s] %s" % (i + 1, matches[i]) for i in xrange(0, len(matches))])
 		print ""
 		pkg_num = raw_input("Package number [1-%s]: " % len(matches))
-		
+
 		if pkg_num.isdigit():
 			pkg_num = int(pkg_num)
 			if pkg_num <= len(matches):
-				return matches[pkg_num-1]
+				return matches[pkg_num - 1]
 		else:
-			return None 
+			return None
 	else:
 		if raw_input("'%s' does not look like a valid package. Add it anyway? [y/N] " % pkg).lower() != "y":
 			return None
@@ -231,13 +241,18 @@ def test_package(package):
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description="Modify local USE flags for packages in /etc/portage/package.use")
-	parser.add_argument('package', type=str, nargs=1, help="Package name: either as 'package' or 'category/package'")
-	parser.add_argument('USE', type=str, nargs=argparse.REMAINDER, help="USE flags to change: +foo -bar -baz")
-	parser.add_argument('-f', '--file', type=str, help="Alternate package.use file (defaults to the prefix-aware etc/portage/package.use)")
+	parser = argparse.ArgumentParser(
+		description="Modify local USE flags for packages in /etc/portage/package.use"
+		)
+	parser.add_argument('package', type=str, nargs=1,
+						help="Package name: either as 'package' or 'category/package'")
+	parser.add_argument('USE', type=str, nargs=argparse.REMAINDER,
+						help="USE flags to change: +foo -bar -baz")
+	parser.add_argument('-f', '--file', type=str,
+						help="Alternate package.use file (defaults to the prefix-aware etc/portage/package.use)")
 	args = parser.parse_args()
 
-	# We cannot exchange this, because positional arguments are only allowed to 
+	# We cannot exchange this, because positional arguments are only allowed to
 	# start with "-" if -- is specified before or they look like neg. numbers.
 	# http://docs.python.org/dev/library/argparse.html#arguments-containing
 	if not args.USE:
@@ -245,10 +260,10 @@ if __name__ == '__main__':
 		sys.exit(1)
 
 	package = test_package(args.package[0])
-	
+
 	if not package:
 		sys.exit(1)
-	
+
 	print "Adding uses %s to package %s" % (', '.join(args.USE), package)
 
 	puh = PackageUSEHandler(args.file)
